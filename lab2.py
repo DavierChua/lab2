02499a37c3c2be6786a7924524be9af599dad5b5
Dav <tan_kwee_teck@ichat.sp.edu.sg>
2024-10-21 10:25:07 +0800

    New calculation for average

--- lab2.py
+++ lab2.py
@@ -1,1 +1,25 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+def user_input():
+    print('Enter some numbers separated by commas: ')
+    txt = input()
+    
+    x = [float(i) for i in txt.split(",")]
+    return x
+
+def calculate_average_temperature(numbers):
+    avg = sum(numbers) / len(numbers)
+    return avg
+def calc_min_max_temperature(numbers):
+    lowest = min(numbers)
+    highest = max(numbers)
+    return lowest, highest
+
+
+numbers_list = user_input()
+min,max= calc_min_max_temperature(numbers_list)
+
+avg1 = calculate_average_temperature(numbers_list)
+
+
+print('Average:', avg1)
+print('Lowest number:', min)
+print('Highest number:', max)
